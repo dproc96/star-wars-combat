33837366a401ef7eaf65f950c2e7e8a1bbab8d98
Dan Proctor <dproc96@gmail.com>
2019-06-08 14:54:18 -0400

    Built out player character selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
 //Define a new class Character with a constructer that makes the attack power, counterattack power, defense, force strength, and health points. Add a getter that randomly generates the result of a roll for attack, counterattack, or force attack.
+var playerCharacter = {};
+var enemyCharacter = {};
+
 $(document).ready(function() {
     class Character {
         constructor(characterName, image, attackPower, attackBonus, attackIncrement, counterAttackPower, counterAttackBonus, defense, forceStrength, healthPoints) {
@@ -38,32 +41,68 @@ $(document).ready(function() {
     //Build four character objects of class Character and push them all into an array
     var characters = {};
     //Object Properties = (name, attackPower, attackBonus, counterAttackPower,counterAttackBonus, defense,forceStrength, healthPoints)
-    characters.luke = new Character("Luke", "<img src='assets/images/luke.png'>", 4, 4, 3, 4, 4, 14, 6, 130);
-    characters.yoda = new Character("Yoda", "<img src='assets/images/yoda.png'>",2, 6, 1, 2, 6, 18, 10, 75);
-    characters.vader = new Character("Darth Vader", "<img src='assets/images/vader.png'>", 5, 3, 2, 5, 3, 16, 7, 100);
-    characters.obiwan = new Character("Obi Wan", "<img src='assets/images/obiwan.png'>", 3, 5, 2, 3, 5, 12, 8, 110);
+    characters.luke = new Character("Luke", "<img id='luke' src='assets/images/luke.png'>", 4, 4, 3, 4, 4, 14, 6, 130);
+    characters.yoda = new Character("Yoda", "<img id='yoda' src='assets/images/yoda.png'>",2, 6, 1, 2, 6, 18, 10, 75);
+    characters.vader = new Character("Darth Vader", "<img id='vader' src='assets/images/vader.png'>", 5, 3, 2, 5, 3, 16, 7, 100);
+    characters.obiwan = new Character("Obi Wan", "<img id='obiwan' src='assets/images/obiwan.png'>", 3, 5, 2, 3, 5, 12, 8, 110);
+
+    
 
     //Generate the default game state.
+    function add(location, elements) {
+        for (let element of elements) {
+            location.append(element);
+        }
+    }
+
+    function generateCharacterCard(name, isPlayerCharacter) {
+        var character = characters[name]
+        $("main").empty();
+        add($("main"),[`<div id='card' class='character-card border__${name}'>`])
+        add($("#card"), [`<div id='name' class='border__${name}'>`, `<div id='image' class='border__${name}'>`, `<div id='stats' class='border__${name}'>`])
+        add($("#name"),[`<h4 class="vertical-center">${character.characterName}</h4>`])
+        add($("#image"),[character.image])
+        var stats = [];
+        if (isPlayerCharacter) {
+            stats.push(`<h4>Attack Bonus: ${character.attackBonus}`);
+            stats.push(`<h4>Base Attack Power: ${character.attackPower}`);
+            stats.push(`<h4>Attack Power Increment: ${character.attackIncrement}`)
+        }
+        else {
+            stats.push(`<h4>Attack Bonus: ${character.counterAttackBonus}`);
+            stats.push(`<h4>Attack Power: ${character.counterAttackPower}`);
+        }
+        stats.push(`<h4>Force Strength: ${character.forceStrength}`);
+        stats.push(`<h4>Defense: ${character.defense}`);
+        stats.push(`<h4>Health: ${character.healthPoints}`)
+        stats.push(`<button id='select' class='border__${name}'>Select</button>`)
+        stats.push(`<button id='return' class='border__${name}'>Return</button>`)
+        add($("#stats"), stats);
+        $("#return").click(function() {
+            if (isPlayerCharacter) {
+                defaultGameState();
+            }
+        })
+        $("#select").click(function() {
+            if(isPlayerCharacter) {
+                for (var key in character) {
+                    playerCharacter[key] = character[key]
+                }
+            }
+        })
+    }
+
     function defaultGameState() {
         $("main").empty();
         $("main").attr("class", "game__player-selection");
-        function add(location, elements) {
-            for (let element of elements) {
-                location.append(element);
-            }
-        }
         add($("main"),["<h4>Choose your fighter!</h4>", "<div class='l-container' id='roster'></div>"]);
         add($("#roster"), [characters.luke.image, characters.yoda.image, characters.vader.image, characters.obiwan.image])
+        $("img").click(function() {
+            generateCharacterCard($(this).attr("id"), true);
+        })
     }
 
     defaultGameState();
-    //1. Populate the roster
-
-    //2. Add click method for player character selection
-
-    //3. Rebuild site accordingly
-
-    //4. Move into new combat setup function
 
 
     //Combat setup function
